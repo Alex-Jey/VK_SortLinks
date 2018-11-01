aab7c8b3d8ef946616db967bc2f60f14c3de2f1d
Alexandr <peshkov.alexandr.net@gmail.com>
2018-11-01 12:54:37 +0300

    Добавьте файлы проекта.

---
+++ VK_SortLinks/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VK_SortLinks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
